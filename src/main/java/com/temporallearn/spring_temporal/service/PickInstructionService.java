f4ae662012dfb904b511fc093dd43957318c1c75
Dakshesh Sharma <dakshesh.s@greyorange.com>
2026-03-24 16:57:35 +0530

    Added dangling_area dna transaction_id

--- src/main/java/com/temporallearn/spring_temporal/service/PickInstructionService.java
+++ src/main/java/com/temporallearn/spring_temporal/service/PickInstructionService.java
@@ -453,8 +453,12 @@ public class PickInstructionService {
                         .userLoggedIn(pi.getUserLoggedIn())
                         .ppsPoint(pi.getPpsPoint())
                         .isMarkedContainerFlow(pi.isMarkedContainerScanned())
-                        .transactionId(tx.getTransactionId())
+                        .transactionId(tx.getContainerAttributes() != null
+                                && tx.getContainerAttributes().getInternalOrderId() != null
+                                ? String.valueOf(tx.getContainerAttributes().getInternalOrderId())
+                                : tx.getTransactionId())
                         .state(tx.getTransactionState())
+                        .danglingArea("bot")
                         .pickedItemInfoList(List.of(itemInfo))
                         .build();
 
